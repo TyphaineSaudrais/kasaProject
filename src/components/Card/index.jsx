354b9e01cc71c25bea1b5f0f39a22915eb7e50e9
Typhaine Prevost <saudraisprevosttyphaine@gmail.com>
2023-04-20 16:55:49 +0200

    changing filter for inear-gradient

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,45 +3,15 @@ import styled from 'styled-components';
 
 
 
-
 const AppartName = styled.p `
-    position: relative;
-    bottom: 80px;
-    left: 10px;
-    width: 50%;
+   
+    width: 60%;
     overflow-wrap: break-word;
+    line-height: center; 
+    margin-left: 15px;
    
 `
-  const LinkAppardCard = styled(Link) `
-  color: #FFFFFF; 
-  text-decoration: none; 
-  & visited {color: #FFFFFF}; 
-  height: 300px;
-  width: 300px;
-  margin: 20px;
-  @media  (max-width: 941px) {
-    width: 80%; 
-    margin: auto;
-    margin-bottom: 20px;
-
-}
-  @media (max-width: 768px) {
-    width: 100%; 
-    margin: 10px auto;
-
-
-}
   
-  `
-
-
-const AppartImg = styled.img `
-    height: 100%;
-    width: 100%;
-    border-radius: 10px;
-    object-fit: cover;
-    filter: contrast(50%);
-    `
 
 
 
@@ -69,21 +39,47 @@ const Card = styled.div `
 `
 
 
-
     
-
 
 
 
 
 function AppartCard ({ id, title , cover }) {
 
+  const LinkAppardCard = styled(Link) `
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+  color: #FFFFFF; 
+  text-decoration: none; 
+  & visited {color: #FFFFFF}; 
+  height: 300px;
+  width: 300px;
+  margin: 20px;
+  border-radius: 10px;
+  background-image: linear-gradient(180deg, 
+    rgba(0, 0, 0, 0), 
+    rgba(0, 0, 0, 0.6)
+  ),  url(${cover});
+  background-size: cover;
+  @media  (max-width: 941px) {
+    width: 80%; 
+    margin: auto;
+    margin-bottom: 20px;
+
+}
+  @media (max-width: 768px) {
+    width: 100%; 
+    margin: 10px auto;
+
+}
+  
+  `
   
 
     return (
     <Card key={id} className='appart-card'>
          <LinkAppardCard to={`/Logements/${id}`}>
-            <AppartImg className='appart-img' src={cover} alt={{title}} />
             <AppartName>{title}</AppartName>
         </LinkAppardCard>
     </Card>
